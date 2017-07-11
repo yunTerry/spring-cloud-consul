a737949a8da2de9010d072f753a0e37bf9b6198e
谈太锐 <2219569347@qq.com>
2017-07-11 16:04:42 +0800

    service consumer

---
+++ service-consumer/src/main/java/com/terry/ServiceConsumerApplication.java
@@ -0,0 +1,12 @@
+package com.terry;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ServiceConsumerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ServiceConsumerApplication.class, args);
+	}
+}
